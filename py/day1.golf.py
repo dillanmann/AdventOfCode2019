59ac316a1a227c26c8af9cb09054991d2a456f84
Dillan Mann <dillan.mann@seebyte.com>
2019-12-03 13:34:34 +0000

    Golfed day 1 a bit more

--- py/day1.golf.py
+++ py/day1.golf.py
@@ -1,5 +1,4 @@
 # run with `cat <input_file.txt> | py day1.golf.py`
-import math
 import sys
 
-print(sum(map(lambda f: math.floor(f/3)-2, [int(m) for m in sys.stdin])))
+print(sum(map(lambda f: int(f/3-2), [int(m) for m in sys.stdin])))
